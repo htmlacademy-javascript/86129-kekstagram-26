fa27e312f8d187e1c89f5a8b7e3ec70f53a0320b
Svetkachalina <svetkachalina@gmail.com>
2022-06-10 19:06:50 +0300

    add 2 functions

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+stringMaxLength(str, max); {
+    if(str.length <= max) {
+        return: true;
+    }
+    return: false;
+}
+    
